077b669a0dd066b4c68825ee1145fd117dffb962
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-06-19 18:53:03 +0530

    changed toggle

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,21 +1,43 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 
-export default class Toggle extends PureComponent {
-  render() {
-    return (
-      <div className="toggle-container">
-        <form className="main-toggle">
-          <input type="radio" id="choice1" name="choice" value="org" />
-          <label htmlFor="choice1">Organisations</label>
+export default function Toggle({ options, onToggle = () => {} }) {
+  const [select, toggleSelect] = useState(options[0].name);
+  const [toggleClass, changeToggleClass] = useState('rotate-toggle');
+  return (
+    <div className="toggle-container">
+      <div className={`main-toggle ${toggleClass}`}>
+        {options.map((o, i) => {
+          return (
+            <React.Fragment key={o.name}>
+              <input
+                onClick={e => {
+                  onToggle(o.value);
+                  toggleSelect(o.name);
+                  changeToggleClass(i ? `rotate-toggle` : 'anti-rotate-toggle');
+                }}
+                defaultChecked={!i}
+                type="radio"
+                id={`choice${i + 1}`}
+                name="choice"
+                value={o.value}
+              />
+              <label htmlFor={`choice${i + 1}`}>
+                {select !== o.name ? o.name : ''}
+              </label>
+            </React.Fragment>
+          );
+        })}
 
-          <input type="radio" id="choice2" name="choice" value="ind" />
-          <label htmlFor="choice2">Individuals</label>
+        {/* <input type="radio" id="choice1" name="choice" value="org" />
+        <label htmlFor="choice1">Organisations</label>
 
-          <div id="flap">
-            <span className="content">Organisations</span>
-          </div>
-        </form>
+        <input type="radio" id="choice2" name="choice" value="ind" />
+        <label htmlFor="choice2">Individuals</label> */}
+
+        <div id="flap">
+          <span className="content">{select}</span>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import Scrollspy from 'react-scrollspy';
 import orgs from '../data/org.json';
 import individual from '../data/individual.json';
@@ -8,70 +8,94 @@ import Footer from '../components/Footer';
 // import Scroll from '../components/Scroll';
 
 import strip1 from '../assets/images/strip11.svg';
-import strip2 from '../assets/images/strip13.svg';
-import strip3 from '../assets/images/strip12.svg';
+import strip2 from '../assets/images/strip12.svg';
+import strip3 from '../assets/images/strip13.svg';
 import strip4 from '../assets/images/strip14.svg';
-import window from '../assets/images/window.svg';
-import icon1 from '../assets/images/icon1.svg';
-import icon2 from '../assets/images/icon2.svg';
-import icon3 from '../assets/images/icon3.svg';
-import icon4 from '../assets/images/icon4.svg';
+// import window from '../assets/images/window.svg';
+// import icon1 from '../assets/images/icon1.svg';
+// import icon2 from '../assets/images/icon2.svg';
+// import icon3 from '../assets/images/icon3.svg';
+// import icon4 from '../assets/images/icon4.svg';
 import Listing from '../components/Listing';
 import Toggle from '../components/Toggle.js';
 
-const IndexPage = () => (
-  <Layout>
-    <div className="body-wrap boxed-container">
-      <Header />
-      <main>
-        <section className="hero text-center full-container">
-          <div className="container-sm">
-            <div className="hero-inner">
-              <h1 className="hero-title h2-mobile mt-0 is-revealing">
-                Monoglot
-              </h1>
-              <p className="hero-paragraph is-revealing">
-                Speaking only one language - Engineering
-              </p>
-              <div className="search-box ">
-                <div className="search-container">
-                  <span className="search-icon">
-                    <i className="fa fa-search" />
-                  </span>
-                  <input type="search" id="search" placeholder="Search" />
-                </div>
-              </div>
-              <Toggle
-                options={[
-                  { name: 'Organisation', value: 'org' },
-                  { name: 'Individual', value: 'ind' },
-                ]}
-              />
-              <div className="hero-browser">
-                <div className="bubble-3 is-revealing">
-                  <img src={strip1} alt="" />
-                </div>
-                <div className="bubble-4 is-revealing">
-                  <img src={strip2} alt="" />
-                </div>
-                <div className="hero-browser-inner is-revealing">
-                  <div className="content">
-                    <Listing entries={individual.entries} />
-                  </div>
-                  {/* <img src={window} alt="" /> */}
-                </div>
-                <div className="bubble-1 is-revealing">
-                  <img src={strip3} alt="" />
-                </div>
-                <div className="bubble-2 is-revealing">
-                  <img src={strip4} alt="" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+export default class IndexPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      source: orgs,
+      sourceName: 'org',
+    };
+  }
 
-        {/* <section className="features section text-center">
+  onSourceChange = sourceName => {
+    this.setState({
+      sourceName,
+      source: sourceName === 'org' ? orgs : individual,
+    });
+  };
+
+  render() {
+    const { source, sourceName } = this.state;
+    return (
+      <Layout>
+        <div className="body-wrap boxed-container">
+          <Header />
+          <main>
+            <section className="hero text-center full-container">
+              <div className="container-sm">
+                <div className="hero-inner">
+                  <h1 className="hero-title h2-mobile mt-0 is-revealing">
+                    Monoglot
+                  </h1>
+                  <p className="hero-paragraph is-revealing">
+                    Speaking only one language - Engineering
+                  </p>
+                  <div className="search-box ">
+                    <div className="search-container">
+                      <span className="search-icon">
+                        <i className="fa fa-search" />
+                      </span>
+                      <input
+                        type="search"
+                        autoComplete="off"
+                        id="search"
+                        placeholder="Search"
+                      />
+                    </div>
+                  </div>
+                  <Toggle
+                    onToggle={name => this.onSourceChange(name)}
+                    options={[
+                      { name: 'Organisation', value: 'org' },
+                      { name: 'Individual', value: 'ind' },
+                    ]}
+                  />
+                  <div className="hero-browser">
+                    <div className="bubble-3 is-revealing">
+                      <img src={strip1} alt="" />
+                    </div>
+                    <div className="bubble-4 is-revealing">
+                      <img src={strip2} alt="" />
+                    </div>
+                    <div className="hero-browser-inner is-revealing">
+                      <div className="content">
+                        <Listing name={sourceName} entries={source.entries} />
+                      </div>
+                      {/* <img src={window} alt="" /> */}
+                    </div>
+                    <div className="bubble-1 is-revealing">
+                      <img src={strip3} alt="" />
+                    </div>
+                    <div className="bubble-2 is-revealing">
+                      <img src={strip4} alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+
+            {/* <section className="features section text-center">
           <div className="container">
             <div className="features-inner section-inner has-bottom-divider">
               <div className="features-wrap">
@@ -130,7 +154,7 @@ const IndexPage = () => (
           </div>
         </section> */}
 
-        <section className="newsletter section">
+            {/* <section className="newsletter section">
           <div className="container-sm">
             <div className="newsletter-inner section-inner">
               <div className="newsletter-header text-center is-revealing">
@@ -160,11 +184,11 @@ const IndexPage = () => (
               </div>
             </div>
           </div>
-        </section>
-      </main>
-      <Footer />
-    </div>
-  </Layout>
-);
-
-export default IndexPage;
+        </section> */}
+          </main>
+          <Footer />
+        </div>
+      </Layout>
+    );
+  }
+}
