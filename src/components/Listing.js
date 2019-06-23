37588b05bdad644bbbc27f7784e3f067e4dbb8d6
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-06-23 14:56:45 +0530

    added listIndex component

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
+import Scroll from './Scroll';
 
 const alphabets26 = '#abcdefghijklmnopqrstuvwxyz';
 const hash = {};
+
+class ListIndex extends Component {
+  render() {
+    const { groups } = this.props;
+    return (
+      <div className="list-index">
+        <div className="list-index-inside">
+          {groups.map(g => {
+            return (
+              <Scroll
+                key={g}
+                type="class"
+                element={`#letter-${g.toUpperCase()}`}
+              >
+                <a href={`#letter-${g.toUpperCase()}`}>{g.toUpperCase()}</a>
+              </Scroll>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
 export default class Listing extends Component {
   alphabeticalOrgs = (name, entries) => {
     if (hash[name]) {
@@ -28,13 +52,20 @@ export default class Listing extends Component {
   render() {
     const { name, entries } = this.props;
     const listItems = this.alphabeticalOrgs(name, entries);
-    const groups = Object.keys(listItems);
+    const groups = Object.keys(listItems).filter(g => listItems[g].length);
+
     return (
       <div className="list">
+        <ListIndex groups={groups} />
         {groups.map(g => {
-          return listItems[g].length ? (
+          return (
             <div className="content-list" key={g}>
-              <div style={{ width: '100%' }}>{g.toUpperCase()}</div>
+              <div
+                className={`#letter-${g.toUpperCase()}`}
+                style={{ margin: '1em 0', fontSize: '2em', width: '100%' }}
+              >
+                {g.toUpperCase()}
+              </div>
               <ul className="feed">
                 {listItems[g].map(o => {
                   return (
@@ -52,7 +83,7 @@ export default class Listing extends Component {
                 })}
               </ul>
             </div>
-          ) : null;
+          );
         })}
       </div>
     );
